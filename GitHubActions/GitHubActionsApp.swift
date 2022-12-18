3fd9cbef7d66acc1a53ae74a6926cb6956790f85
Cagri Gider <cagri.gider@adesso.com.tr>
2022-12-18 14:51:32 +0300

    Project initalized

---
+++ GitHubActions/GitHubActionsApp.swift
@@ -0,0 +1,17 @@
+//
+//  GitHubActionsApp.swift
+//  GitHubActions
+//
+//  Created by Cagri Gider on 18.12.2022.
+//
+
+import SwiftUI
+
+@main
+struct GitHubActionsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
