479a195cddc3e1d24c9806a6c7ecbe87396a8c5b
Cagri Gider <cagri.gider@adesso.com.tr>
2022-12-19 23:19:13 +0300

    swiftlint added

---
+++ GitHubActions/Pods/Target Support Files/Pods-GitHubActions-GitHubActionsUITests/Pods-GitHubActions-GitHubActionsUITests-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_Pods_GitHubActions_GitHubActionsUITests : NSObject
+@end
+@implementation PodsDummy_Pods_GitHubActions_GitHubActionsUITests
+@end
